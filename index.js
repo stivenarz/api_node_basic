0892450f9fcd8b945a65686d927b5338b0c4cd25
snarz <ingeariza@live.com>
2024-10-25 00:25:41 -0500

    se modifica el port

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-const port = 3000
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('<h1 style="text-align: center;">Hello, World!</h1>'))
+app.get('/', (req, res) => {
+    console.log('end point / entrance')
+    res.send('<h1 style="text-align: center;">Hello, World!</h1>')
+})
 app.listen(port, () => console.log(`api_node_basic listening on port ${port}!`))
